131715a406d6b0dd9566f480f3d81c3dbaf16745
saka <869621380@qq.com>
2025-11-11 15:00:01 +0800

    HTTP请求会话协议处理

---
+++ src/main/java/com/saka/gateway/session/SessionChannelInitializer.java
@@ -0,0 +1,22 @@
+package com.saka.gateway.session;
+
+import com.saka.gateway.session.handler.SessionServerHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+
+public class SessionChannelInitializer  extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        pipeline.addLast(new HttpRequestDecoder());
+        pipeline.addLast(new HttpResponseEncoder());
+        pipeline.addLast(new HttpObjectAggregator(1024*1024));
+        pipeline.addLast(new SessionServerHandler());
+    }
+}
---
+++ src/main/java/com/saka/gateway/session/SessionServer.java
@@ -0,0 +1,48 @@
+package com.saka.gateway.session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.Channel;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Callable;
+
+
+public class SessionServer implements Callable<Channel> {
+
+    private final Logger logger = LoggerFactory.getLogger(SessionServer.class);
+
+    private final EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+    private final EventLoopGroup workerGroup = new NioEventLoopGroup();
+    private  Channel serverChannel;
+
+    @Override
+    public Channel call() throws Exception {
+        ChannelFuture channelFuture = null;
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class) // 指定服务端通道类型
+                    .option(ChannelOption.SO_BACKLOG,128)// 设置 TCP 积压队列大小
+                    .childHandler(new SessionChannelInitializer());
+            channelFuture = bootstrap.bind(new InetSocketAddress(7397)).syncUninterruptibly();
+            this.serverChannel = channelFuture.channel();
+        } catch (Exception e) {
+            logger.error("socket server start error.", e);
+        }finally {
+            if (null != channelFuture && channelFuture.isSuccess())
+                logger.info("socket server start done.");
+           else
+               logger.error("socket server start error.");
+        }
+
+        return serverChannel;
+    }
+}
